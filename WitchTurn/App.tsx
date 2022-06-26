064e4c31ef03c61ade109a894e0db37f8b41c974
ScriptkidHicks <tammashicks@gmail.com>
2022-06-25 21:03:23 -0700

    laying the groundwork of the app

---
+++ WitchTurn/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app, Tammas!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
