384cb1e7c4dc54686ac0bd24328f9efc30c05ddf
ScriptkidHicks <tammashicks@gmail.com>
2022-07-11 16:15:04 -0700

    updating turn mockup

--- WitchTurn/App.js
+++ WitchTurn/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Gobo from "./assets/Pictures/GoboTest.png";
+
+import TurnTaker from "./Components/TurnTaker";
 
 export default function App() {
+  const [turnTakersList, setTurnTakersList] = useState([
+    {
+      name: "Persephone",
+      Initiative: 12,
+      imageSource: Gobo,
+      Bonus: 1,
+    },
+  ]);
+
   return (
     <View style={styles.container}>
+      <Text style={styles.TitleBar}>Witch Turn</Text>
+      <View style={styles.TurnContainer}>
+        {turnTakersList.map((character, index) => {
+          return (
+            <TurnTaker
+              name={character.name}
+              imageSource={character.imageSource}
+              Initiative={character.Initiative}
+              Bonus={character.Bonus}
+              key={(character.Initiative, character.nameTag)}
+            />
+          );
+        })}
+      </View>
       <StatusBar style="light" />
-      <Text style={styles.introTitle}>Witch Turn</Text>
-      <TouchableOpacity style={styles.openButton}>
-        <Text style={styles.openText}>Begin Tracking</Text>
-      </TouchableOpacity>
     </View>
   );
 }
@@ -16,27 +39,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#111",
+    backgroundColor: "#1a1a1a",
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
     alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 60,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
   },
-  introTitle: {
+
+  TitleBar: {
+    width: "100%",
     color: "white",
-    fontSize: 42,
-    fontWeight: "bold",
-    marginBottom: 30,
-  },
-  openText: {
-    color: "black",
+    justifyContent: "center",
+    alignItems: "center",
     textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
+    fontSize: 24,
+    paddingBottom: 20,
   },
-  openButton: {
-    backgroundColor: "white",
-    padding: 20,
+
+  TurnContainer: {
+    backgroundColor: "#1f1f1f",
+    width: "100%",
+    height: "100%",
     alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 30,
   },
 });
---
+++ WitchTurn/Components/TurnTaker.js
@@ -0,0 +1,100 @@
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+
+function TurnTaker(props) {
+  return (
+    <View style={styles.TurnTakerBody}>
+      <View style={styles.TurnTakerPictureContainer}>
+        <Image source={props.imageSource} style={styles.TurnTakerPicture} />
+      </View>
+
+      <View style={styles.TurnTakerInfo}>
+        <View style={styles.InfoLeft}>
+          <Text style={styles.NameTag}>{props.name}</Text>
+          <Text style={styles.Initiative}>
+            {"Initiative: "}
+            {props.Initiative}
+          </Text>
+          <Text style={styles.Bonus}>
+            {props.Bonus >= 0
+              ? "Bonus: +" + props.Bonus
+              : "Bonus: " + props.Bonus}
+          </Text>
+        </View>
+        <View style={styles.InfoRight}></View>
+      </View>
+
+      <TouchableOpacity>
+        <View style={styles.DeleteButton}>
+          <Text style={styles.DeleteText}>X</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default TurnTaker;
+
+const styles = StyleSheet.create({
+  TurnTakerBody: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#333",
+    marginBottom: 20,
+    flexDirection: "row",
+    borderRadius: 20,
+    alignItems: "center",
+    padding: 10,
+  },
+
+  TurnTakerPictureContainer: {
+    width: "15%",
+  },
+
+  TurnTakerPicture: {
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+  },
+
+  TurnTakerInfo: {
+    width: "72%",
+    height: "100%",
+    paddingLeft: 10,
+  },
+
+  NameTag: {
+    color: "white",
+  },
+
+  Initiative: {
+    color: "white",
+  },
+
+  Bonus: {
+    color: "white",
+  },
+
+  InfoLeft: {
+    height: "100%",
+  },
+
+  InfoRight: {
+    height: "100%",
+  },
+
+  DeleteButton: {
+    backgroundColor: "black",
+    color: "white",
+    height: 35,
+    width: 35,
+    borderRadius: 40,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  DeleteText: {
+    color: "white",
+    fontSize: 24,
+  },
+});
